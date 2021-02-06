67df4ca4e306685a90fc5a3a0d13fc2adaf578dc
Vlad Dr.Frame <remenjuk2010@gmail.com>
2021-02-06 08:20:40 +0200

    minor fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const _ = require('lodash');
 
 import refs from './js/refs';
 import fetchCountries from './js/fetchCountries';
-import pnotifyPop from './js/components/pnotify';
 import * as util from './js/update-articles-markup';
 
 refs.inputRef.addEventListener('input', _.debounce(handleQuerry, 500));
@@ -13,32 +12,12 @@ refs.btnRef.addEventListener('click', util.handleClearMarkup);
 function handleQuerry(event) {
   util.handleClearMarkup();
   const query = event.target.value;
-  let arrayLength = 0;
 
   if (query === '') {
     return;
   }
 
-  fetchCountries(query)
-    .then(res => res.json())
-    .then(data => {
-      arrayLength = data.length;
-      return data;
-    })
-    .then(data => {
-      if (arrayLength >= 2 && arrayLength < 11) {
-        util.updateCountryListMarkup(data);
-      }
-      if (arrayLength === 1) {
-        util.updateCountryMarkup(data);
-      }
-      if (arrayLength >= 10) {
-        pnotifyPop();
-      }
-
-      arrayLength = 0;
-    })
-    .catch(error => console.log('Ошибка'));
+  fetchCountries(query);
 }
 
 function handleInputClear() {
